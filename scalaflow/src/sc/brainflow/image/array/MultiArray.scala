5b6cba53860ff86698f16356381f8a7b6a26c9fe
unknown <brad.buchsbaum@gmail.com>
2010-01-25 21:43:59 -0500

    various and sundry

---
+++ scalaflow/src/sc/brainflow/image/array/MultiArray.scala
@@ -0,0 +1,20 @@
+package sc.brainflow.image.array
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Brad
+ * Date: Jan 24, 2010
+ * Time: 10:02:16 PM
+ * To change this template use File | Settings | File Templates.
+ */
+
+trait MultiArray[T] {
+
+  
+
+
+
+
+
+
+}
